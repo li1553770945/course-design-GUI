7c0d707fc64457d9e294d93dbda9b208b2b143ba
李 亚宁 <1553770945@qq.com>
2020-04-22 11:20:12 +0800

    添加了单元测试，原来感觉没用的文件现在感觉好像挺有用，重新添加回来

---
+++ UnitTestGlobal/UnitTestGlobal.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../h/global.h"
+#include "../cpp/global.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestGlobal
+{
+	TEST_CLASS(UnitTestGlobal)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Assert::AreEqual(123, my_atoi("123"));
+			Assert::AreEqual(12.5, my_atof("12.5"));
+			Assert::AreEqual(-1.0, my_atof("double"));
+		}
+	};
+}
